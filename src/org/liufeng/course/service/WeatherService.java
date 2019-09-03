787d1a76fa2a7ab2e189dc0b2693055684df21d2
milotian <milotian1988@gmail.com>
2019-09-03 21:51:30 +0800

    update weather api

--- src/org/liufeng/course/service/WeatherService.java
+++ src/org/liufeng/course/service/WeatherService.java
@@ -6,6 +6,9 @@ import java.util.regex.Pattern;
 
 import org.liufeng.course.util.HttpRequestUtil;
 
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
 
 /**
  * 天气预报服务
@@ -36,12 +39,14 @@ public class WeatherService {
 	 * @param source
 	 * @return
 	 */
-	public static String getWeatherXml(String source,int day) {
+	public static String getWeatherJson(String source ) {
 		String dst = null;
 		// 组装查询地址
-		String requestUrl = "http://php.weather.sina.com.cn/xml.php?city={keyWord}&password=DJOYnieT8234jlsK&day="+day;
+//		String requestUrl = "http://php.weather.sina.com.cn/xml.php?city={keyWord}&password=DJOYnieT8234jlsK&day="+day;
+		String requestUrl = "http://api.map.baidu.com/telematics/v3/weather?location={keyWord}&output=json&ak=8OjaGkLpzgRlTIHvP1E8mtdX";
 		// 对参数q的值进行urlEncode utf-8编码
-		requestUrl = requestUrl.replace("{keyWord}", HttpRequestUtil.urlEncode(source, "GBK"));
+		requestUrl = requestUrl.replace("{keyWord}", source);
+//		System.out.print(requestUrl);
 		dst = HttpRequestUtil.httpRequest(requestUrl);
 		return dst;
 	}
@@ -53,50 +58,18 @@ public class WeatherService {
 	 */
 	public static String getWeatherInfo(String source) {
 		StringBuffer buffer = new StringBuffer();
+		String jsonResult = getWeatherJson(source);
 		buffer.append(source).append(" 今明后三天天气情况如下：\n\n");
-		for(int i=0;i<3;i++) {
-			String weatherXml = getWeatherXml(source,i);
-			if(null==weatherXml || "".equals(weatherXml))
-				return "";
-			String status1 = "";
-			String direction1 = "";
-			String temperature1 = "";
-			String temperature2 = ""; 
-			String savedate_weather = "";
-			String ssd_l = "";
-			String yd_s = "";
-			Pattern p = Pattern.compile("(.*)(<status1>)(.*?)(</status1>)(.*)");
-			Matcher m = p.matcher(weatherXml);
-			if(m.matches())
-				status1 = m.group(3);
-			if(null==status1 || "".endsWith(status1))
-				return "";
-			p = Pattern.compile("(.*)(<direction1>)(.*?)(</direction1>)(.*)");
-			m = p.matcher(weatherXml);
-			if(m.matches())
-				direction1 = m.group(3);
-			p = Pattern.compile("(.*)(<temperature1>)(.*?)(</temperature1>)(.*)");
-			m = p.matcher(weatherXml);
-			if(m.matches())
-				temperature1 = m.group(3);
-			p = Pattern.compile("(.*)(<temperature2>)(.*?)(</temperature2>)(.*)");
-			m = p.matcher(weatherXml);
-			if(m.matches())
-				temperature2 = m.group(3);
-			p = Pattern.compile("(.*)(<savedate_weather>)(.*?)(</savedate_weather>)(.*)");
-			m = p.matcher(weatherXml);
-			if(m.matches())
-				savedate_weather = m.group(3);
-			p = Pattern.compile("(.*)(<ssd_l>)(.*?)(</ssd_l>)(.*)");
-			m = p.matcher(weatherXml);
-			if(m.matches())
-				ssd_l = m.group(3);
-			p = Pattern.compile("(.*)(<yd_s>)(.*?)(</yd_s>)(.*)");
-			m = p.matcher(weatherXml);
-			if(m.matches())
-				yd_s = m.group(3);
-			buffer.append(savedate_weather).append("\n").append(status1).append(" ").append(direction1).append(" ").append(temperature2)
-				.append("°-").append(temperature1).append("° ").append(ssd_l).append("\n").append("温馨提示：").append(yd_s).append("\n\n");
+		JSONArray respContent =  JSONObject.fromObject(jsonResult).getJSONArray("results");
+		JSONArray weatherdata = respContent.getJSONObject(0).getJSONArray("weather_data");
+		System.out.println(respContent);
+		System.out.println(weatherdata);
+		for(int i=0; i< weatherdata.size();i++) {
+			JSONObject daydata = weatherdata.getJSONObject(i);
+			buffer.append("日期:").append(daydata.getString("date")).append(" ")
+			.append("天气:").append(daydata.getString("weather")).append(" ")
+			.append("风力:").append(daydata.getString("wind")).append(" ")
+			.append("温度:").append(daydata.getString("temperature")).append("\n");
 		}
 		return (null==buffer?"":buffer.toString());
 	}
